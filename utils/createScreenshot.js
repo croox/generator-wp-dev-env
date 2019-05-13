840cd0f03c51cbc35be6e6a0065022ca379f03eb
jhotadhari <tellme@waterproof-webdesign.info>
2019-05-13 09:06:26 +0530

    Fix create screenshot from figlet
    add missing file ;)

---
+++ utils/createScreenshot.js
@@ -0,0 +1,86 @@
+const chalk = require('chalk');
+const figlet = require('figlet');
+const im = require('imagemagick');
+
+const createScreenshot = ( generator, inputText ) => {
+
+	// ??? requires ImageMagick, does it break all if no ImageMagick?
+
+	const content = 'text 0,0 "' + figlet.textSync( inputText, {
+		font: 'Ogre',
+		horizontalLayout: 'fitted',
+		verticalLayout: 'fitted'
+	} ) + '"';
+
+	const dest = generator.destinationPath( 'src/screenshot.png' );
+
+	const size = '1200x900';
+	const margin = 10;	// %
+	const sizeWithMargin = () => size.split('x').map( pxs => pxs - ( ( margin / 100 ) * pxs ) ).join('x');
+
+	[
+		{
+			// create huge wide image with big letters
+			cmd: 'convert',
+			args: [
+				'-size','3500x800',
+				'xc:white',
+				'-font','FreeMono',
+				'-pointsize','110',
+				'-fill','black',
+				'-gravity','center',
+				'-draw',content,
+				dest,
+			],
+		},
+		{
+			// crop img to content and resize to smaller than final size
+			cmd: 'convert',
+			args: [
+				dest,
+				'-trim',
+				'+repage',
+				'-resize',sizeWithMargin() + '>',
+				'-gravity','center',
+				'-background','white',
+				'-extent',sizeWithMargin(),
+				dest,
+			],
+		},
+		{
+			// resize image canvas to final size, center content
+			cmd: 'convert',
+			args: [
+				dest,
+				'-resize',size + '>',
+				'-gravity','center',
+				'-background','white',
+				'-extent',size,
+				dest,
+			],
+		},
+	].reduce( ( accumulatorPromise, process ) => {
+		return accumulatorPromise.then( () => {
+			return new Promise( ( resolve, reject ) => {
+				im[process.cmd]( process.args, ( err, stdout ) => {
+					if ( err )
+						reject( err );
+					resolve( stdout );
+				} );
+			} );
+		} ).catch( err => console.log( err ) );
+	}, Promise.resolve() ).then( result => {
+		generator.log('');
+		generator.log( chalk.green( 'Added screenshot' ) );
+		generator.log('');
+	} ).catch( err => {
+		generator.log('');
+		generator.log( chalk.red( 'Couldn\'t create Screenshot' ) );
+		generator.log( chalk.red( 'Is ImageMagick installed?' ) );
+		generator.log( err );
+		generator.log('');
+	} );
+
+};
+
+module.exports = createScreenshot;
