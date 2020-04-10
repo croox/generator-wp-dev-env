b631132c50fbef2726ed6a5c1e48a83e62cdc930
jhotadhari <tellme@waterproof-webdesign.info>
2020-04-10 07:11:11 +0530

    Remove header_textcolor from customizer, beacause .site-title a not existing anymore

--- generators/app/template_collections/underboots/src/classes/Customizer.php
+++ generators/app/template_collections/underboots/src/classes/Customizer.php
@@ -36,7 +36,6 @@ class Customizer {
 	public function register( $wp_customize ) {
 		$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
 		$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
-		$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
 	}
 
 	/**
