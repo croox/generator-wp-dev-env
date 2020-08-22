c2e57bfc6fcc7667e38425554b5ebff53d8ca39a
jhotadhari <tellme@waterproof-webdesign.info>
2020-08-22 11:31:18 +0530

    Theme tenplates, add_theme_support: Get color values dynamically

--- generators/app/template_collections/underboots/src/inc/fun/_add_theme_support.php
+++ generators/app/template_collections/underboots/src/inc/fun/_add_theme_support.php
@@ -14,6 +14,179 @@ if ( ! isset( $content_width ) ) {
 	$content_width = 640; /* pixels */
 }
 
+use croox\wde\utils\Color;
+use croox\wde\utils\Arr;
+
+if ( ! function_exists ( '<%= funcPrefix %>_get_editor_color_palette_assoc' ) ) {
+	/**
+	 * Customize Block Color Palettes
+	 *
+	 * See
+	 *	- Bootstrap variables												src/scss/<%= funcPrefix %>_frontend/variables_site/_variables_bootstrap.scss
+	 *	- Add background and text color classes for theme-color-palette 	src/scss/<%= funcPrefix %>_frontend/_generic.scss
+	 *	- Add background gradients classes for editor_gradient_presets		src/scss/<%= funcPrefix %>_frontend/_generic.scss
+	 *	- https://developer.wordpress.org/block-editor/developers/themes/theme-support/#block-color-palettes
+	 */
+	function <%= funcPrefix %>_get_editor_color_palette_assoc() {
+		$css_file_base = '<%= funcPrefix %>_frontend';
+		$editor_color_palette_assoc = array(
+			// bootstrap colors
+			'primary'	=> array(
+				'name' => __( 'Primary', '<%= textDomain %>' ),
+				'slug' => 'primary',
+				'color' => <%= funcPrefix %>_get_css_property( 'primary', $css_file_base ),
+			),
+			'secondary'	=> array(
+				'name' => __( 'Secondary', '<%= textDomain %>' ),
+				'slug' => 'secondary',
+				'color' => <%= funcPrefix %>_get_css_property( 'secondary', $css_file_base ),
+			),
+
+			/* theme-colors */
+			'success'	=> array(
+				'name' => __( 'Success', '<%= textDomain %>' ),
+				'slug' => 'success',
+				'color' => <%= funcPrefix %>_get_css_property( 'success', $css_file_base ),
+			),
+			'info'	=> array(
+				'name' => __( 'Info', '<%= textDomain %>' ),
+				'slug' => 'info',
+				'color' => <%= funcPrefix %>_get_css_property( 'info', $css_file_base ),
+			),
+			'warning'	=> array(
+				'name' => __( 'Warning', '<%= textDomain %>' ),
+				'slug' => 'warning',
+				'color' => <%= funcPrefix %>_get_css_property( 'warning', $css_file_base ),
+			),
+			'danger'	=> array(
+				'name' => __( 'Danger', '<%= textDomain %>' ),
+				'slug' => 'danger',
+				'color' => <%= funcPrefix %>_get_css_property( 'danger', $css_file_base ),
+			),
+			'light'	=> array(
+				'name' => __( 'Light', '<%= textDomain %>' ),
+				'slug' => 'light',
+				'color' => <%= funcPrefix %>_get_css_property( 'light', $css_file_base ),
+			),
+			'dark'	=> array(
+				'name' => __( 'Dark', '<%= textDomain %>' ),
+				'slug' => 'dark',
+				'color' => <%= funcPrefix %>_get_css_property( 'dark', $css_file_base ),
+			),
+
+			/* colors */
+			'blue'	=> array(
+				'name' => __( 'Blue', '<%= textDomain %>' ),
+				'slug' => 'blue',
+				'color' => <%= funcPrefix %>_get_css_property( 'blue', $css_file_base ),
+			),
+			'indigo'	=> array(
+				'name' => __( 'Indigo', '<%= textDomain %>' ),
+				'slug' => 'indigo',
+				'color' => <%= funcPrefix %>_get_css_property( 'indigo', $css_file_base ),
+			),
+			'purple'	=> array(
+				'name' => __( 'Purple', '<%= textDomain %>' ),
+				'slug' => 'purple',
+				'color' => <%= funcPrefix %>_get_css_property( 'purple', $css_file_base ),
+			),
+			'pink'	=> array(
+				'name' => __( 'Pink', '<%= textDomain %>' ),
+				'slug' => 'pink',
+				'color' => <%= funcPrefix %>_get_css_property( 'pink', $css_file_base ),
+			),
+			'red'	=> array(
+				'name' => __( 'Red', '<%= textDomain %>' ),
+				'slug' => 'red',
+				'color' => <%= funcPrefix %>_get_css_property( 'red', $css_file_base ),
+			),
+			'orange'	=> array(
+				'name' => __( 'Orange', '<%= textDomain %>' ),
+				'slug' => 'orange',
+				'color' => <%= funcPrefix %>_get_css_property( 'orange', $css_file_base ),
+			),
+			'yellow'	=> array(
+				'name' => __( 'Yellow', '<%= textDomain %>' ),
+				'slug' => 'yellow',
+				'color' => <%= funcPrefix %>_get_css_property( 'yellow', $css_file_base ),
+			),
+			'green'	=> array(
+				'name' => __( 'Green', '<%= textDomain %>' ),
+				'slug' => 'green',
+				'color' => <%= funcPrefix %>_get_css_property( 'green', $css_file_base ),
+			),
+			'teal'	=> array(
+				'name' => __( 'Teal', '<%= textDomain %>' ),
+				'slug' => 'teal',
+				'color' => <%= funcPrefix %>_get_css_property( 'teal', $css_file_base ),
+			),
+			'cyan'	=> array(
+				'name' => __( 'Cyan', '<%= textDomain %>' ),
+				'slug' => 'cyan',
+				'color' => <%= funcPrefix %>_get_css_property( 'cyan', $css_file_base ),
+			),
+
+			/* grays */
+			'white'	=> array(
+				'name' => __( 'White', '<%= textDomain %>' ),
+				'slug' => 'white',
+				'color' => <%= funcPrefix %>_get_css_property( 'white', $css_file_base ),
+			),
+			'gray'	=> array(
+				'name' => __( 'Gray', '<%= textDomain %>' ),
+				'slug' => 'gray',
+				'color' => <%= funcPrefix %>_get_css_property( 'gray', $css_file_base ),
+			),
+			'black'	=> array(
+				'name' => __( 'Black', '<%= textDomain %>' ),
+				'slug' => 'black',
+				'color' => <%= funcPrefix %>_get_css_property( 'black', $css_file_base ),
+			),
+
+		);
+
+		// Remove duplicate colors
+		$existing_colors = array();
+		$editor_color_palette_assoc = array_filter( $editor_color_palette_assoc, function( $col ) use ( &$existing_colors ) {
+			if ( in_array( $col['color'], $existing_colors ) )
+				return false;
+			$existing_colors[] = $col['color'];
+			return true;
+		} );
+
+		return $editor_color_palette_assoc;
+	}
+}
+
+if ( ! function_exists ( '<%= funcPrefix %>_get_editor_gradient_presets' ) ) {
+	/**
+	 * Example: Configure the predefined set of gradients.
+	 * See function <%= funcPrefix %>_add_theme_support `add_theme_support( 'editor-gradient-presets', $editor_gradient_presets );`
+	 */
+	function <%= funcPrefix %>_get_editor_gradient_presets( $editor_color_palette_assoc = null ) {
+		if ( null === $editor_color_palette_assoc )
+			$editor_color_palette_assoc = <%= funcPrefix %>_get_editor_color_palette_assoc();
+		return Color::build_editor_gradient_presets( array(
+			array(
+				'type' => 'linear',
+				'deg' => '90',
+				'steps' => array(
+					// 		slug 			alpha 	step
+					array( 'primary', 		1,		0 ),
+					array( 'secondary', 	1, 		100 ),
+				),
+			),
+			array(
+				'type' => 'radial',
+				'steps' => array(
+					// 		slug 			alpha 	step
+					array( 'white',			1, 		0 ),
+					array( 'black', 		1, 		100 ),
+				),
+			),
+		), $editor_color_palette_assoc );
+	}
+}
 
 if ( ! function_exists ( '<%= funcPrefix %>_add_theme_support' ) ) {
 	/**
@@ -58,206 +231,28 @@ if ( ! function_exists ( '<%= funcPrefix %>_add_theme_support' ) ) {
 		 */
 		add_theme_support( 'post-thumbnails' );
 
-		/*
-		 * Adding support for Widget edit icons in customizer
-		 */
-		add_theme_support( 'customize-selective-refresh-widgets' );
-
-		/*
-		 * Enable support for Post Formats.
-		 * See http://codex.wordpress.org/Post_Formats
-		 */
-		add_theme_support( 'post-formats', array(
-			'aside',
-			'image',
-			'video',
-			'quote',
-			'link',
-		) );
-
-		// Set up the WordPress core custom background feature.
-		add_theme_support( 'custom-background', apply_filters( '<%= funcPrefix %>_custom_background_args', array(
-			'default-color' => 'ffffff',
-			'default-image' => '',
-		) ) );
-
 		// Set up the WordPress Theme logo feature.
 		add_theme_support( 'custom-logo' );
 
 		// Add support for responsive embedded content.
 		add_theme_support( 'responsive-embeds' );
 
-		// Customize Block Color Palettes
-		// hard coded copy of scss variables
-		// See
-		//	- Bootstrap variables												src/scss/<%= funcPrefix %>_frontend/variables_site/_variables_bootstrap.scss
-		//	- Add background and text color classes for theme-color-palette 	src/scss/<%= funcPrefix %>_frontend/_generic.scss
-		//	- Add background gradients classes for editor_gradient_presets		src/scss/<%= funcPrefix %>_frontend/_generic.scss
-		// https://developer.wordpress.org/block-editor/developers/themes/theme-support/#block-color-palettes
-		$editor_color_palette_assoc = array(
-			// bootstrap colors
-			'primary'	=> array(
-				'name' => __( 'Primary', '<%= textDomain %>' ),
-				'slug' => 'primary',
-				'color' => '#007bff',
-			),
-			'secondary'	=> array(
-				'name' => __( 'Secondary', '<%= textDomain %>' ),
-				'slug' => 'secondary',
-				'color' => '#6c757d',
-			),
-
-			'blue'	=> array(
-				'name' => __( 'Blue', '<%= textDomain %>' ),
-				'slug' => 'blue',
-				'color' => '#007bff',
-			),
-			'indigo'	=> array(
-				'name' => __( 'Indigo', '<%= textDomain %>' ),
-				'slug' => 'indigo',
-				'color' => '#6610f2',
-			),
-			'purple'	=> array(
-				'name' => __( 'Purple', '<%= textDomain %>' ),
-				'slug' => 'purple',
-				'color' => '#6f42c1',
-			),
-			'pink'	=> array(
-				'name' => __( 'Pink', '<%= textDomain %>' ),
-				'slug' => 'pink',
-				'color' => '#e83e8c',
-			),
-			'red'	=> array(
-				'name' => __( 'Red', '<%= textDomain %>' ),
-				'slug' => 'red',
-				'color' => '#dc3545',
-			),
-			'orange'	=> array(
-				'name' => __( 'Orange', '<%= textDomain %>' ),
-				'slug' => 'orange',
-				'color' => '#fd7e14',
-			),
-			'yellow'	=> array(
-				'name' => __( 'Yellow', '<%= textDomain %>' ),
-				'slug' => 'yellow',
-				'color' => '#ffc107',
-			),
-			'green'	=> array(
-				'name' => __( 'Green', '<%= textDomain %>' ),
-				'slug' => 'green',
-				'color' => '#28a745',
-			),
-			'teal'	=> array(
-				'name' => __( 'Teal', '<%= textDomain %>' ),
-				'slug' => 'teal',
-				'color' => '#20c997',
-			),
-			'cyan'	=> array(
-				'name' => __( 'Cyan', '<%= textDomain %>' ),
-				'slug' => 'cyan',
-				'color' => '#17a2b8',
-			),
-			// bootstrap grays
-			'white'	=> array(
-				'name' => __( 'White', '<%= textDomain %>' ),
-				'slug' => 'white',
-				'color' => '#ffffff',
-			),
-			'gray-100'	=> array(
-				'name' => __( 'Gray 100', '<%= textDomain %>' ),
-				'slug' => 'gray-100',
-				'color' => '#f8f9fa',
-			),
-			'gray-200'	=> array(
-				'name' => __( 'Gray 200', '<%= textDomain %>' ),
-				'slug' => 'gray-200',
-				'color' => '#e9ecef',
-			),
-			'gray-300'	=> array(
-				'name' => __( 'Gray 300', '<%= textDomain %>' ),
-				'slug' => 'gray-300',
-				'color' => '#dee2e6',
-			),
-			'gray-400'	=> array(
-				'name' => __( 'Gray 400', '<%= textDomain %>' ),
-				'slug' => 'gray-400',
-				'color' => '#ced4da',
-			),
-			'gray-500'	=> array(
-				'name' => __( 'Gray 500', '<%= textDomain %>' ),
-				'slug' => 'gray-500',
-				'color' => '#adb5bd',
-			),
-			'gray-600'	=> array(
-				'name' => __( 'Gray 600', '<%= textDomain %>' ),
-				'slug' => 'gray-600',
-				'color' => '#6c757d',
-			),
-			'gray-700'	=> array(
-				'name' => __( 'Gray 700', '<%= textDomain %>' ),
-				'slug' => 'gray-700',
-				'color' => '#495057',
-			),
-			'gray-800'	=> array(
-				'name' => __( 'Gray 800', '<%= textDomain %>' ),
-				'slug' => 'gray-800',
-				'color' => '#343a40',
-			),
-			'gray-900'	=> array(
-				'name' => __( 'Gray 900', '<%= textDomain %>' ),
-				'slug' => 'gray-900',
-				'color' => '#212529',
-			),
-			'black'	=> array(
-				'name' => __( 'Black', '<%= textDomain %>' ),
-				'slug' => 'black',
-				'color' => '#000000',
-			),
-		);
+		$editor_color_palette_assoc = <%= funcPrefix %>_get_editor_color_palette_assoc();
 		add_theme_support( 'editor-color-palette', array_values( $editor_color_palette_assoc ) );
 
 		/**
-		 * Example: Configure the predefined set of gradients.
-		 * And add inline style, generic css classes.
+		 * Example: Add editor-gradient-presets and inline style for generic css classes.
+		 * Configure them in function <%= funcPrefix %>_get_editor_gradient_presets
 		 */
 		/*
-		if ( method_exists( 'croox\wde\utils\Color', 'build_editor_gradient_presets' )
-			&& method_exists( 'croox\wde\utils\Color', 'build_gradient_presets_css' )
-		) {
-			$editor_gradient_presets = utils\Color::build_editor_gradient_presets( array(
-				array(
-					'type' => 'linear',
-					'deg' => '90',
-					'steps' => array(
-						// 		slug 			alpha 	step
-						array( 'primary', 		1,		0 ),
-						array( 'secondary', 	1, 		100 ),
-					),
-				),
-				array(
-					'type' => 'radial',
-					'steps' => array(
-						// 		slug 			alpha 	step
-						array( 'white',			1, 		0 ),
-						array( 'black', 		1, 		100 ),
-					),
-				),
-			), $editor_color_palette_assoc );
-
-			add_theme_support(
-				'editor-gradient-presets',
-				$editor_gradient_presets
+		$editor_gradient_presets = <%= funcPrefix %>_get_editor_gradient_presets( $editor_color_palette_assoc );
+		add_theme_support( 'editor-gradient-presets', $editor_gradient_presets );
+		add_action( is_admin() ? 'admin_enqueue_scripts' : 'wp_enqueue_scripts', function() use ( $editor_gradient_presets ) {
+			wp_add_inline_style(
+				is_admin() ? '<%= funcPrefix %>_editor' : '<%= funcPrefix %>_frontend',
+				Color::build_gradient_presets_css( $editor_gradient_presets )
 			);
-
-			// Add inline style, generic css classes, for $editor_gradient_presets
-			$gradient_presets_css = utils\Color::build_gradient_presets_css( $editor_gradient_presets );
-			add_action( 'wp_enqueue_scripts', function() use ( $gradient_presets_css ) {
-				wp_add_inline_style( '<%= funcPrefix %>_frontend', $gradient_presets_css );
-			}, 20 );
-			add_action( 'admin_enqueue_scripts', function() use ( $gradient_presets_css ) {
-				wp_add_inline_style( '<%= funcPrefix %>_editor', $gradient_presets_css );
-			}, 20 );
-		}
+		}, 20 );
 		*/
 
 		// Add Support For Block Editor Alignments
